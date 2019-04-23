178b709420a87d822770bb678f386831ad9f5e98
georgeshanti <georgeshanti@gmail.com>
2019-04-23 23:41:33 +0530

    Indentation Fixed

--- decorator.hpp
+++ decorator.hpp
@@ -3,42 +3,42 @@
 
 template<class returnType, class... argumentTypes>
 class Decorator{
-    protected:
-    public:
-        typedef returnType (*decorateeFunc)(argumentTypes...);
-        Decorator<returnType,argumentTypes...>* _decorateeObj;
-        decorateeFunc _decorateeFunc;
+	protected:
+	public:
+		typedef returnType (*decorateeFunc)(argumentTypes...);
+		Decorator<returnType,argumentTypes...>* _decorateeObj;
+		decorateeFunc _decorateeFunc;
 
-        void operator=(const Decorator &D ) {
-            _decorateeObj = D._decorateeObj;
-            _decorateeFunc = D._decorateeFunc;
-        }
+		void operator=(const Decorator &D ) {
+			_decorateeObj = D._decorateeObj;
+			_decorateeFunc = D._decorateeFunc;
+		}
 
-        Decorator(){
-            _decorateeFunc = 0;
-            _decorateeObj = 0;
-        }
+		Decorator(){
+			_decorateeFunc = 0;
+			_decorateeObj = 0;
+		}
 
-        Decorator(decorateeFunc dF){
-            _decorateeFunc = dF;
-            _decorateeObj = 0;
-        }
+		Decorator(decorateeFunc dF){
+			_decorateeFunc = dF;
+			_decorateeObj = 0;
+		}
 
-        Decorator(Decorator<returnType,argumentTypes...> &D){
-            _decorateeFunc = 0;
-            _decorateeObj = &D;
-        }
+		Decorator(Decorator<returnType,argumentTypes...> &D){
+			_decorateeFunc = 0;
+			_decorateeObj = &D;
+		}
 
-        returnType decoratee(argumentTypes... a){
-            if(_decorateeFunc)
-                return (*_decorateeFunc)(a...);
-            else
-                return (*_decorateeObj)(a...);
-        }
-        
-        virtual returnType operator()(argumentTypes... a){
-            return _decorateeFunc(a...);
-        };
+		returnType decoratee(argumentTypes... a){
+			if(_decorateeFunc)
+				return (*_decorateeFunc)(a...);
+			else
+				return (*_decorateeObj)(a...);
+		}
+		
+		virtual returnType operator()(argumentTypes... a){
+			return _decorateeFunc(a...);
+		};
 };
 
 #endif
--- example.cpp
+++ example.cpp
@@ -2,22 +2,22 @@
 #include <iostream>
 
 class NewDecorator : public Decorator<int,int>{
-    public:
-        NewDecorator():Decorator(){};
-        NewDecorator(decorateeFunc dF):Decorator(dF){};
-        NewDecorator(NewDecorator &D):Decorator(D){};
-        int operator () (int a){
-            return decoratee(a)+2;
-        }
+	public:
+		NewDecorator():Decorator(){};
+		NewDecorator(decorateeFunc dF):Decorator(dF){};
+		NewDecorator(NewDecorator &D):Decorator(D){};
+		int operator () (int a){
+			return decoratee(a)+2;
+		}
 };
 
 int t(int a){
-    return a;
+	return a;
 }
 
 int main(){
-    NewDecorator A(t);
-    NewDecorator B(A);
-    std::cout<<t(3)<<std::endl<<A(3)<<std::endl<<B(3)<<std::endl;
-    return 0;
+	NewDecorator A(t);
+	NewDecorator B(A);
+	std::cout<<t(3)<<std::endl<<A(3)<<std::endl<<B(3)<<std::endl;
+	return 0;
 }
