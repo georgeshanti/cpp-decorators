d1ff96ec8b4c39e6207e39869ff3994762a017f2
georgeshanti <georgeshanti@gmail.com>
2019-04-24 00:34:18 +0530

    Readme and example modified to reflect different decorator propogation

--- example.cpp
+++ example.cpp
@@ -5,9 +5,19 @@ class NewDecorator : public Decorator<int,int>{
 	public:
 		NewDecorator():Decorator(){};
 		NewDecorator(decorateeFunc dF):Decorator(dF){};
-		NewDecorator(NewDecorator &D):Decorator(D){};
+		NewDecorator(Decorator &D):Decorator(D){};
 		int operator () (int a){
 			return decoratee(a)+2;
+		}
+};
+
+class NewDecorator1 : public Decorator<int,int>{
+	public:
+		NewDecorator1():Decorator(){};
+		NewDecorator1(decorateeFunc dF):Decorator(dF){};
+		NewDecorator1(Decorator &D):Decorator(D){};
+		int operator () (int a){
+			return decoratee(a)+3;
 		}
 };
 
@@ -17,7 +27,7 @@ int t(int a){
 
 int main(){
 	NewDecorator A(t);
-	NewDecorator B(A);
+	NewDecorator1 B(A);
 	std::cout<<t(3)<<std::endl<<A(3)<<std::endl<<B(3)<<std::endl;
 	return 0;
 }
